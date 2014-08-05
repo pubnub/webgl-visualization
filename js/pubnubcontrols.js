28dd4f37199dc0c87e87b95eb61eadc10ddccb23
Stephen L. Blum <stephen@pubnub.com>
2014-08-04 22:44:11 -0700

    Update pubnubcontrols.js

--- js/pubnubcontrols.js
+++ js/pubnubcontrols.js
@@ -21,8 +21,8 @@
   }
 
   var p = PUBNUB.init({
-    publish_key   : "demo",
-    subscribe_key : "demo"
+        "publish_key"           : "pub-c-5f15e90d-f3f8-4daa-a223-1ea114d2fb28",
+        "subscribe_key"         : "sub-c-4f71d6ee-1c63-11e4-a1db-02ee2ddab7fe"
   });
 
   p.subscribe({
