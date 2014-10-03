939b1ccb07db95d799dc44d333b0a3c25b5d348e
Stephen L. Blum <blum.stephen@gmail.com>
2014-10-03 15:03:07 -0700

    Create url-config.js

---
+++ js/url-config.js
@@ -0,0 +1,34 @@
+(function(){
+
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Get URL Params
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+function urlparams() {
+    var params = {};
+    if (location.href.indexOf('?') < 0) return params;
+
+    PUBNUB.each(
+        location.href.split('?')[1].split('&'),
+        function(data) { var d = data.split('='); params[d[0]] = d[1]; }
+    );
+
+    return params;
+}
+
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Main
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+funciton main() {
+    var params = urlparams();
+    if (!('hide' in params)) return;
+    PUBNUB.each( params['hide'].split(','), function(id) {
+        PUBNUB.css( PUBNUB.$(id), { display : 'none' } );
+    } );
+}
+
+main();
+
+
+})();
