6f85560a55f4d912d0b5908e5915f1c6d2b63b0c
Stephen Blum <blum.stephen@gmail.com>
2023-07-18 14:50:09 -0700

    fixed mixed content errors and added line animations.

--- js/data.js
+++ js/data.js
@@ -16,6 +16,41 @@ pubnub.subscribe({
     timeStamps = timeStamps.concat(msg.geo_map);
   }
 });
+setInterval(()=>{
+    timeStamps = timeStamps.concat([{"lat":r()[0],"lng":r()[1],"geos":[r(),r(),r(),r()]}]);
+},500);
+
+function r() {
+    var latitude = Math.random() * (90.0 - -90.0) + -90.0;
+    var longitude = Math.random() * (180.0 - -180.0) + -180.0;
+    return [+latitude.toFixed(6), +longitude.toFixed(6)];
+}
+
+
+function r2() {
+    // Define bounding boxes for known countries
+    var landAreas = [
+        { lat: [36.1, 45.5], long: [-83.7, -116.4] }, // USA
+        { lat: [20.6, 55.9], long: [-62.4, -172.9] }, // Canada
+        { lat: [-33.8, -54.6], long: [112.0, 159.1] }, // Australia
+        { lat: [35.8, 45.7], long: [130.3, 153.9] }, // Japan
+        { lat: [36.0, 42.1], long: [-9.5, -3.2] }, // Spain
+        { lat: [41.8, 47.5], long: [26.0, 40.2] }, // Romania
+        { lat: [-22.7, -35.3], long: [14.2, 51.1] } // South Africa
+        // Add more bounding boxes as needed
+    ];
+
+    // Pick a random land area
+    var randomAreaIndex = Math.floor(Math.random() * landAreas.length);
+    var randomArea = landAreas[randomAreaIndex];
+
+    // Generate a random latitude and longitude within the selected land area
+    var latitude = Math.random() * (randomArea.lat[1] - randomArea.lat[0]) + randomArea.lat[0];
+    var longitude = Math.random() * (randomArea.long[1] - randomArea.long[0]) + randomArea.long[0];
+
+    return [+latitude.toFixed(6), +longitude.toFixed(6)];
+}
+
 var k;
 var z = setInterval(function() {
   var x = exPubSub(timeStamps);
